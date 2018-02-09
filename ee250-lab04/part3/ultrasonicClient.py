d9e02199cd35e9a54592bc0acbf63b1d6e3c7756
CS104 Student User <hailey@usc.edu>
2018-02-08 16:42:20 -0800

    To the Pi!

--- ee250-lab04/part3/ultrasonicClient.py
+++ ee250-lab04/part3/ultrasonicClient.py
@@ -4,5 +4,30 @@ import sys
 sys.path.append('../../Software/Python/')
 
 from grovepi import *
+import socket
 
+def Main():
+    # Change the host and port as needed. For ports, use a number in the 9000 
+    # range. 
+    host = '192.168.1.217'
+    port = 8000
+
+    server_addr = '10.0.2.15'
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    s.bind((host,port))
+
+     # UDP is connectionless, so a client does not formally connect to a server
+    # before sending a message.
+    dst_port = input("destination port-> ")
+
+    while True:
+    	#tuples are immutable so we need to overwrite the last tuple
+        server = (server_addr, int(dst_port))
+        s.sendto((grovepi.ultrasonicRead(ultrasonic_ranger)).encode('utf-8'), server)
+    s.close()
+
+
+if __name__ == '__main__':
+    Main()
 #use UDP
--- ee250-lab04/part3/ultrasonicServer.py
+++ ee250-lab04/part3/ultrasonicServer.py
@@ -4,5 +4,23 @@ import sys
 sys.path.append('../../Software/Python/')
 
 from grovepi import *
+import socket 
+
+
+def serverProcess():
+    host = '10.0.2.15'
+    port = 9000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host,port))
+    print ("Ultrasonic Server Process Started")
+    while True:
+        data, addr = s.recvfrom(1024)
+        data = data.decode('utf-8')
+        print(data)
+    s.close()	#changed from c to s
+
 
 #use UDP
+if __name__ == '__main__':
+    serverProcess()
